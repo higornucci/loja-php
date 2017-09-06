8ac822e8dd7f997ee2487bcf5a44d80f43f1867c
unknown <higor.nucci@dominio.ufms.br>
2017-09-06 15:43:55 -0300

    iniciando projeto php para uma loja virtual

---
+++ adiciona-produto.php
@@ -0,0 +1,19 @@
+<?php include("cabecalho.php"); ?>
+<?php include("conecta.php"); ?>
+<?php include("banco-produto.php"); ?>
+<?php
+
+$nome = $_GET["nome"];
+$preco = $_GET["preco"];
+
+if(insereProduto($conexao, $nome, $preco)) { ?>
+    <p class="text-success">O produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
+<?php } else {
+    $msg = mysqli_error($conexao);
+?>
+    <p class="text-danger">O produto <?= $nome; ?> não foi adicionado: <?= $msg ?></p>
+<?php
+}
+?>
+
+<?php include("rodape.php"); ?>
---
+++ cabecalho.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+    <title>Minha loja</title>
+    <meta charset="utf-8">
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/loja.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="index.php">Minha Loja</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="produto-formulario.php">Adiciona Produto</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="produto-lista.php">Produtos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="sobre.php">Sobre</a>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+    </div>
+
+    <div class="container">
+
+    <div class="principal">
