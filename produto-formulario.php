52fce9e1640d4665e1ac18b368d19febe7b2edde
unknown <higor.nucci@dominio.ufms.br>
2017-09-08 07:57:45 -0400

    seleção de categorias para um produto

--- produto-formulario.php
+++ produto-formulario.php
@@ -20,11 +20,17 @@ $categorias = listaCategorias($conexao);
             <td><textarea name="descricao" class="form-control"></textarea></td>
         </tr>
         <tr>
+            <td></td>
+            <td><input type="checkbox" name="usado" value="true"> Usado</td>
+        </tr>
+        <tr>
             <td>Categoria</td>
             <td>
-                <?php foreach($categorias as $categoria) : ?>
-                    <input type="radio" name="categoria_id" value="<?=$categoria['id']?>"><?=$categoria['nome']?></br>
-                <?php endforeach ?>
+                <select name="categoria_id">
+                    <?php foreach($categorias as $categoria) : ?>
+                        <option value="<?=$categoria['id']?>"><?=$categoria['nome']?></option>
+                    <?php endforeach ?>
+                </select>
             </td>
         </tr>
         <tr>
