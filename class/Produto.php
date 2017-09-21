e75a8e88c3f15e98858cec7a7ce31f79227f043a
unknown <higor.nucci@dominio.ufms.br>
2017-09-21 07:36:20 -0400

    criação de métodos para o calculo do desconto

--- class/Produto.php
+++ class/Produto.php
@@ -14,4 +14,9 @@ class Produto
     public $descricao;
     public $categoria;
     public $usado;
+
+    public function precoComDesconto($valor)
+    {
+        return $this->preco - ($this->preco * $valor);
+    }
 }
